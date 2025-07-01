41d9391f5947ff6d791b9e24f395f8c4d59be7f4
EM_Strava_Coach <emmanuel.maguerez@gmail.com>
2025-07-01 08:27:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -179,4 +179,4 @@ def profile():
     return render_template('profile.html', profile=profile)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
